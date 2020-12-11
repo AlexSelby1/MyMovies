a0e33c2b0fc379cc6cbd3537eeff9ce5f1476ed7
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2020-12-11 10:19:28 +0000

    Added methods responisble for displaying global list of the movies and movie by id

--- src/main/java/com/mycompany/mymovies1/resources/MovieResource.java
+++ src/main/java/com/mycompany/mymovies1/resources/MovieResource.java
@@ -2,10 +2,45 @@
  */
 package com.mycompany.mymovies1.resources;
 
+import com.mycompany.mymovies1.model.Movie;
+import com.mycompany.mymovies1.service.MovieService;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author Alex Selby, Conor Dixon, Lukasz Plawinski
  */
+@Path("/movies")
 public class MovieResource {
+    MovieService movieService = new MovieService();
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Movie> getListOfMoviesXML(){
+        return movieService.getAllMovies();
+    }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Movie> getListOfMoviesJSON(){
+        return movieService.getAllMovies();
+    }
+    
+    @GET
+    @Path ("/{movieID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Movie getMovieJSON(@PathParam("movieID") int id){
+        return movieService.getMovies(id);
+    }
+    
+    @GET        
+    @Path ("/{movieID}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Movie getMovieXML(@PathParam("movieID") int id){
+        return movieService.getMovies(id);
+    }
 }
