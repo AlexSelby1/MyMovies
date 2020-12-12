95c66347f6439c07b93e6a5ad443b8110a1c9e48
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2020-12-12 13:04:37 +0000

    Created post method for adding movie to the personal movie list

--- src/main/java/com/mycompany/mymovies1/resources/AccountResource.java
+++ src/main/java/com/mycompany/mymovies1/resources/AccountResource.java
@@ -2,10 +2,12 @@
  */
 package com.mycompany.mymovies1.resources;
 import com.mycompany.mymovies1.model.Account;
+import com.mycompany.mymovies1.model.Movie;
 import com.mycompany.mymovies1.service.AccountService;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,4 +43,9 @@ public class AccountResource {
         return accountService.getAccount(id);
 }
 
+    @POST
+    @Path ("/{accountID}/addMovie/{movieID}")
+    public String addMovie(@PathParam("movieID")long movieID,@PathParam("accountID") long accountID){
+        return accountService.addMovie(movieID,accountID);
+    }
 }
