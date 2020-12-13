1b964b24238ebeb0ed6139801e5752b34ea5d5a7
Lukasz Plawinski <plawinski.lukasz@gmail.com>
2020-12-13 10:53:42 +0000

    Code cleanup

--- src/main/java/com/mycompany/mymovies1/resources/CustomerResource.java
+++ src/main/java/com/mycompany/mymovies1/resources/CustomerResource.java
@@ -1,5 +1,3 @@
-/*
- */
 package com.mycompany.mymovies1.resources;
 import com.mycompany.mymovies1.model.Account;
 import com.mycompany.mymovies1.model.Customer;
@@ -22,48 +20,57 @@ import javax.ws.rs.QueryParam;
  */
 @Path("/customers")
 public class CustomerResource {
+//    Objects
     CustomerService customerService = new CustomerService();
      AccountService accountService = new AccountService();
+     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Customer> getCustomersJSON(){
         return customerService.getAllCustomers();
     }
+    
     @GET
     @Path ("/{customerID}")
     @Produces(MediaType.APPLICATION_JSON)
     public Customer getCustomersJSON(@PathParam("customerID") int id){
         return customerService.getCustomer(id);
-}
+    }
+    
     @GET
     @Produces(MediaType.APPLICATION_XML)
     public List<Customer> getCustomerXML(){
         return customerService.getAllCustomers();
     }
+    
     @GET
     @Path ("/{customerID}")
     @Produces(MediaType.APPLICATION_XML)
     public Customer getCustomerXML(@PathParam("customerID") int id){
         return customerService.getCustomer(id);
-}
-        @GET
+    }
+    
+    @GET
     @Path ("/{customerID}/accounts")
     @Produces(MediaType.APPLICATION_XML)
     public List<Account> getAllAccountsbyCustomerXML(@PathParam("customerID") int id){
         return customerService.getAllAccountsByCustomer(id);
-}
+    }
+    
     @GET
     @Path ("/{customerID}/accounts")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Account> getAllAccountsbyCustomerJSON(@PathParam("customerID") int id){
         return customerService.getAllAccountsByCustomer(id);
-}
+    }
+    
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path ("/{customerID}/create")
     public Account createAccount(Account acc){
         return accountService.createAccount(acc);
     }
+    
     @PUT
     @Path ("/{customerID}/transferMovie/{movieID}")
     public Movie transferMovie(@PathParam("movieID") long movieID,@QueryParam("fromID") long fromID,@QueryParam("toID") long toID){
